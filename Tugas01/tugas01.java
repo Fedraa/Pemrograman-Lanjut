c45a3be56044a9cca4c963358ae364dfe6026d40
Fedraa <ghaffarfedra1@gmail.com>
2024-02-23 08:44:43 +0700

    Initial Commit 3.0

--- Tugas01/tugas01.java
+++ Tugas01/tugas01.java
@@ -4,6 +4,7 @@ public class tugas01{
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("Kampret");
+        System.out.println("Seperti mati lampu ya sayang");
     }
     
 }
