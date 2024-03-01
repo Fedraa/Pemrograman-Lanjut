69637ed4da811b8eaa8a09fa6e9b13802ac1ab96
Fedraa <ghaffarfedra1@gmail.com>
2024-03-01 07:56:47 +0700

    1 Maret

---
+++ Praktikum/src/perkuliahantgl1bln3/Mobil.java
@@ -0,0 +1,15 @@
+package perkuliahantgl1bln3;
+
+    public class Mobil {
+        public String merk;
+        public String platNomor;
+        public int tahun;
+        public String transmisi;
+
+        //methods(behaviour)
+        public void maju(){
+            System.out.printf("Mobil %s dengan plat nomor %s bergerak maju\n"); this.merk, this.platNomor;
+        }
+    }
+   
+    
---
+++ Praktikum/src/perkuliahantgl1bln3/mainMobil.java
@@ -0,0 +1,12 @@
+package perkuliahantgl1bln3;
+
+public class mainMobil {
+    public static void main(String[] args) {
+        Mobil mobil1 = new Mobil();
+
+        mobil1.merk = "Innova Reborn";
+        mobil1.platNomor = "N 1987 H";
+    }
+    
+    
+}
