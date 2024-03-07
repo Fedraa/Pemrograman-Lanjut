e0b1f2e75b1b25a3645d6c637127dc6a6c4dcd95
Fedraa <ghaffarfedra1@gmail.com>
2024-03-07 20:14:33 +0700

    Tugas Perpus update 20.14 07/03/2024

--- Praktikum/src/tugasPerpus/perpusDataBase.java
+++ Praktikum/src/tugasPerpus/perpusDataBase.java
@@ -24,34 +24,85 @@ package tugasPerpus;
             {"Sejarah Kebudayaan Islam", "Husin Umar", "Gema Insani Press", "2016", "978-979-2713-07-5", "358 halaman", "Rp98.000"},
             {"Sejarah Revolusi Indonesia", "Soekarno", "Penerbit Djambatan", "2009", "978-979-428-496-5", "528 halaman", "Rp115.000"}
         };
-            String[][] daftarBukuAgama = {
+        String[][] daftarBukuAgama = {
             {"Al-Qur'an dan Terjemahnya", "Tim Penerjemah Departemen Agama RI", "Kementerian Agama RI", "Berbagai edisi", "-", "-", "Rp50.000"},
             {"Bhagavadgita: Kitab Pesan Ilahi", "Anonim", "Penerbit Kompas", "2013", "978-979-709-887-3", "352 halaman", "Rp75.000"},
             {"Tao Te Ching: Kitab Suci Kebijaksanaan", "Laozi", "Gramedia Pustaka Utama", "2018", "978-602-03-6243-8", "192 halaman", "Rp60.000"},
             {"Menemukan Makna Hidup: Sebuah Pencarian Spiritual", "Maria Ananda", "Gema Pustaka", "2018", "978-9876543210", "250 halaman", "Rp120.000"},
             {"Membangun Kehidupan Spiritual yang Kokoh", "John Doe", "Pustaka Hikmah", "2020", "978-1234567890", "200 halaman", "Rp100.000"}
         };
-            String[][] daftarBukuPsikologi = {
+        String[][] daftarBukuPsikologi = {
             {"Psikologi Abnormal: Konsep dan Aplikasi Modern","David H. Barlow","Erlangga","2019","978-602-471-225-4","400 halaman","Rp150.000"},
             {"Psikologi Kepribadian: Pendekatan Teori dan Praktik","Brent W. Roberts & Robert C. Lea","Kencana","2020","978-602-421-633-3","350 halaman","Rp135.000"},
             {"Psikologi Sosial: Teori dan Terapan","Elliot Aronson, Timothy D. Wilson & Robin M. Akert","Salemba Humanika","2018","978-979-065-344-3","380 halaman","Rp145.000"},
             {"Psikologi Industri dan Organisasi: Teori dan Praktik","Paul E. Spector","PT RajaGrafindo Persada","2017","978-602-423-350-7","320 halaman","Rp130.000"},
             {"Psikologi Kognitif: Teori, Penelitian, dan Aplikasi","John R. Anderson","Erlangga","2021","978-602-523-678-1","420 halaman","Rp155.000"}
         };
-            String[][] daftarBukuPolitik = {
+        String[][] daftarBukuPolitik = {
             {"Politik Identitas: Konflik dan Dinamika dalam Masyarakat Multikultural","Arief Budiman","Kompas Media Nusantara","2020","978-979-709-510-5","320 halaman","Rp125.000"},
             {"Politik Global: Dinamika Persaingan Antar Kekuatan Dunia","Joseph S. Nye Jr.","Yayasan Pustaka Obor Indonesia","2019","978-979-461-959-9","280 halaman","Rp135.000"},
             {"Politik dan Ekonomi: Perspektif Kritis","Noam Chomsky","Mizan Pustaka","2018","978-602-317-506-4","380 halaman","Rp145.000"},
             {"Teori Politik: Konsep Dasar dan Pengembangan Terkini","Andrew Heywood","PT Penerbit Buku Kompas","2021","978-602-322-927-9","420 halaman","Rp150.000"},
             {"Politik Hukum: Teori dan Praktik dalam Sistem Peradilan","Keith E. Whittington","Yayasan Pustaka Obor Indonesia","2017","978-979-461-506-5","350 halaman","Rp140.000"}
         };
-            String[][] daftarBukuFiksi = {
+        String[][] daftarBukuFiksi = {
             {"Pulang","Tere Liye","Gramedia Pustaka Utama","2016","978-602-03-2239-3","400 halaman","Rp85.000"},
             {"Laskar Pelangi","Andrea Hirata","Bentang Pustaka","2005","978-979-1227-08-7","368 halaman","Rp95.000"},
             {"Perahu Kertas","Dee Lestari","Bentang Pustaka","2009","978-979-1227-09-4","400 halaman","Rp90.000"},
             {"Sang Pemimpi","Andrea Hirata","Bentang Pustaka","2006","978-979-1227-04-9","360 halaman","Rp100.000"},
             {"5 cm","Donny Dhirgantoro","Grasindo","2011","978-979-22-7192-3","326 halaman","Rp88.000"}
         };
+        String inventaris (){
+            String dataInventaris = "";
+            System.out.println();
+            System.out.println(
+                "     Data Inventaris     "
+                + "\n~~~~Perpustakaan Amba~~~~"
+                + "\n\nJumlah kategori Buku: 7 kategori {Teknologi, Filsafat, Sejarah, Agama, Psikologi, Politik, Fiksi}"
+                + "\nJumlah buku per-kategori: 5 buku"
+                + "\nJumlah keseluruhan buku di perpustakaan: 35 Buku "
+            );
+            return dataInventaris;
+        }
+        String selamatDatang (){
+            String welcome = "";
+            System.out.println();
+            System.out.println(
+                "     Selamat datang di     "
+                + "\n~~~~~Perpustakaan Amba~~~~~"
+                + "\n\nBerikut daftar kategori buku yang terdapat di Perpustakaan Amba"
+                + "\n\n1. Teknologi"
+                + "\n2. Filsafat"
+                + "\n3. Sejarah" 
+                + "\n4. Agama" 
+                + "\n5. Psikologi" 
+                + "\n6. Politik"
+                + "\n7. Fiksi"
+                + "\n\nAnda dapat memasukkan kategori buku yang anda inginkan sesuai dengan angka yang tertera"
+                + "\natau Anda dapat melihat data inventaris Perpustakaan Amba dengan memasukkan angka:"
+                + "\n8. Tampilkan inventaris perpustakaan\n");
+            System.out.print("Masukkan nomor menu yang Anda inginkan: "
+            );
+            return welcome;
+        }
+
+
+    
+        public String interfaceKategori (String[][] buku) {
+            String result = "";
+            for (int i = 1; i <= 5; i++) {
+                result += "\nBuku ke-" + i;
+                result +=
+                 "\n\nJudul Buku: " + buku[i - 1][0] 
+                        + "\nPenulis: " + buku[i - 1][1]
+                        + "\nPenerbit: " + buku[i - 1][2]
+                        + "\nTahun Terbit: " + buku[i - 1][3]
+                        + "\nISBN: " + buku[i - 1][4]
+                        + "\nJumlah Halaman: " + buku[i-1][5]
+                        + "\nHarga: " + buku[i-1][6] + "\n";
+            }
+            return result;
+        }
 
 
     
--- Praktikum/src/tugasPerpus/perpusUI.java
+++ Praktikum/src/tugasPerpus/perpusUI.java
@@ -9,123 +9,32 @@ public class perpusUI {
         String menuLain;
 
         do {
-            System.out.println();
-            System.out.println(
-                "     Selamat datang di     "
-                + "\n~~~~~Perpustakaan Amba~~~~~"
-                + "\n\nBerikut daftar kategori buku yang terdapat di Perpustakaan Amba"
-                + "\n\n1. Teknologi"
-                + "\n2. Filsafat"
-                + "\n3. Sejarah" 
-                + "\n4. Agama" 
-                + "\n5. Psikologi" 
-                + "\n6. Politik"
-                + "\n7. Fiksi"
-                + "\n\nAnda dapat memasukkan kategori buku yang anda inginkan sesuai dengan angka yang tertera"
-                + "\natau Anda dapat melihat data inventaris Perpustakaan Amba dengan memasukkan angka:"
-                + "\n8. Tampilkan inventaris perpustakaan\n");
-            System.out.print("Masukkan nomor menu yang Anda inginkan: ");
+            System.out.print(buku.selamatDatang());
             kategori = input.nextInt();
             switch (kategori) {
                 case 1:
-                    for (int i = 1; i <= 5; i++) {
-                        System.out.println("\nBuku ke-" + i);
-                        System.out.println(
-                        "\nJudul Buku: " + buku.daftarBukuTeknologi[i - 1][0] 
-                        + "\nPenulis: " + buku.daftarBukuTeknologi[i - 1][1]
-                        + "\nPenerbit: " + buku.daftarBukuTeknologi[i - 1][2]
-                        + "\nTahun Terbit: " + buku.daftarBukuTeknologi[i - 1][3]
-                        + "\nISBN: " + buku.daftarBukuTeknologi[i - 1][4]
-                        + "\nJumlah Halaman: " + buku.daftarBukuTeknologi[i-1][5]
-                        + "\nHarga: " + buku.daftarBukuTeknologi[i-1][6]);
-                    }
+                    System.out.println( buku.interfaceKategori(buku.daftarBukuTeknologi));
                     break;
                 case 2:
-                    for (int i = 1; i <= 5; i++) {
-                        System.out.println("\nBuku ke-" + i);
-                        System.out.println(
-                        "\nJudul Buku: " + buku.daftarBukuFilsafat[i - 1][0] 
-                        + "\nPenulis: " + buku.daftarBukuFilsafat[i - 1][1]
-                        + "\nPenerbit: " + buku.daftarBukuFilsafat[i - 1][2]
-                        + "\nTahun Terbit: " + buku.daftarBukuFilsafat[i - 1][3]
-                        + "\nISBN: " + buku.daftarBukuFilsafat[i - 1][4]
-                        + "\nJumlah Halaman: " + buku.daftarBukuFilsafat[i-1][5]
-                        + "\nHarga: " + buku.daftarBukuFilsafat[i-1][6]);
-                    }
+                    System.out.println( buku.interfaceKategori(buku.daftarBukuFilsafat));
                     break;
                 case 3:
-                    for (int i = 1; i <= 5; i++) {
-                        System.out.println("\nBuku ke-" + i);
-                        System.out.println(
-                            "\nJudul Buku: " + buku.daftarBukuSejarah[i - 1][0] 
-                            + "\nPenulis: " + buku.daftarBukuSejarah[i - 1][1]
-                            + "\nPenerbit: " + buku.daftarBukuSejarah[i - 1][2]
-                            + "\nTahun Terbit: " + buku.daftarBukuSejarah[i - 1][3]
-                            + "\nISBN: " + buku.daftarBukuSejarah[i - 1][4]
-                            + "\nJumlah Halaman: " + buku.daftarBukuSejarah[i-1][5]
-                            + "\nHarga: " + buku.daftarBukuSejarah[i-1][6]);
-                    }
+                    System.out.println( buku.interfaceKategori(buku.daftarBukuSejarah));
                     break;
                 case 4:
-                    for (int i = 1; i <= 5; i++) {
-                        System.out.println("\nBuku ke-" + i);
-                        System.out.println(
-                            "\nJudul Buku: " + buku.daftarBukuAgama[i - 1][0] 
-                            + "\nPenulis: " + buku.daftarBukuAgama[i - 1][1]
-                            + "\nPenerbit: " + buku.daftarBukuAgama[i - 1][2]
-                            + "\nTahun Terbit: " + buku.daftarBukuAgama[i - 1][3]
-                            + "\nISBN: " + buku.daftarBukuAgama[i - 1][4]
-                            + "\nJumlah Halaman: " + buku.daftarBukuAgama[i-1][5]
-                            + "\nHarga: " + buku.daftarBukuAgama[i-1][6]);
-                    }
+                    System.out.println( buku.interfaceKategori(buku.daftarBukuAgama));
                     break;
                 case 5:
-                    for (int i = 1; i <= 5; i++) {
-                        System.out.println("\nBuku ke-" + i);
-                        System.out.println(
-                            "\nJudul Buku: " + buku.daftarBukuPsikologi[i - 1][0] 
-                            + "\nPenulis: " + buku.daftarBukuPsikologi[i - 1][1]
-                            + "\nPenerbit: " + buku.daftarBukuPsikologi[i - 1][2]
-                            + "\nTahun Terbit: " + buku.daftarBukuPsikologi[i - 1][3]
-                            + "\nISBN: " + buku.daftarBukuPsikologi[i - 1][4]
-                            + "\nJumlah Halaman: " + buku.daftarBukuPsikologi[i-1][5]
-                            + "\nHarga: " + buku.daftarBukuPsikologi[i-1][6]);
-                    }
+                    System.out.println( buku.interfaceKategori(buku.daftarBukuPsikologi));
                     break;
                 case 6:
-                    for (int i = 1; i <= 5; i++) {
-                        System.out.println("\nBuku ke-" + i);
-                        System.out.println(
-                            "\nJudul Buku: " + buku.daftarBukuPolitik[i - 1][0] 
-                            + "\nPenulis: " + buku.daftarBukuPolitik[i - 1][1]
-                            + "\nPenerbit: " + buku.daftarBukuPolitik[i - 1][2]
-                            + "\nTahun Terbit: " + buku.daftarBukuPolitik[i - 1][3]
-                            + "\nISBN: " + buku.daftarBukuPolitik[i - 1][4]
-                            + "\nJumlah Halaman: " + buku.daftarBukuPolitik[i-1][5]
-                            + "\nHarga: " + buku.daftarBukuPolitik[i-1][6]);
-                    }
+                    System.out.println( buku.interfaceKategori(buku.daftarBukuPolitik));
                     break;
                 case 7:
-                    for (int i = 1; i <= 5; i++) {
-                        System.out.println("\nBuku ke-" + i);
-                        System.out.println(
-                            "\nJudul Buku: " + buku.daftarBukuFiksi[i - 1][0] 
-                            + "\nPenulis: " + buku.daftarBukuFiksi[i - 1][1]
-                            + "\nPenerbit: " + buku.daftarBukuFiksi[i - 1][2]
-                            + "\nTahun Terbit: " + buku.daftarBukuFiksi[i - 1][3]
-                            + "\nISBN: " + buku.daftarBukuFiksi[i - 1][4]
-                            + "\nJumlah Halaman: " + buku.daftarBukuFiksi[i-1][5]
-                            + "\nHarga: " + buku.daftarBukuFiksi[i-1][6]);
-                    }
+                    System.out.println( buku.interfaceKategori(buku.daftarBukuPolitik));
                     break;
                 case 8:
-                    System.out.println(
-                        "     Data Inventaris     "
-                        + "\n~~~~Perpustakaan Amba~~~~"
-                        + "\n\nJumlah kategori Buku: 7 kategori {Teknologi, Filsafat, Sejarah, Agama, Psikologi, Politik, Fiksi}"
-                        + "\nJumlah buku per-kategori: 5 buku"
-                        + "\nJumlah keseluruhan buku di perpustakaan: 35 Buku "
-                    );
+                    System.out.println(buku.inventaris());
                     break;
                 default:
                     System.out.println("Pilihan tidak valid. Silakan coba lagi.");
@@ -136,7 +45,7 @@ public class perpusUI {
         } while (menuLain.equalsIgnoreCase("ya"));
             if (menuLain.equalsIgnoreCase("tidak")){
                     System.out.println("\n~~~Terima kasih telah mengunjungi Perpustakaan Amba~~~");
-            }
+            } 
         input.close();
     }
 }
